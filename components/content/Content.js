0aa18101c29dca7aff3d993d045898d4be358219
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-28 20:17:19 +0000

    revamped intro

--- components/content/Content.js
+++ components/content/Content.js
@@ -1,21 +1,25 @@
-// import styles from './Footer.module.css'
+import styles from "./Content.module.css";
 
 // TODO: rotate between random phrases.
 export default function Content() {
   return (
     <>
-      <section>
+      <section className={styles.content}>
         <p>
-            I'm Matthew Bidewell, I'm a Software engineer with 7 years of experience in creating well crafted
-            SaaS solutions using AWS. I work over at Government Digital Services where I help with Digital Identification. 🧑‍💻
+          I'm Matthew Bidewell, I'm a Software engineer with 7 years of
+          experience in creating well crafted SaaS solutions using AWS. I work
+          over at Government Digital Services where I help with Digital
+          Identification. 🧑‍💻
         </p>
         <p>
-            I'm also passionate about cyber security and you can find me hacking away at Hack the Box. 📦
+          I'm also passionate about cyber security and you can find me hacking
+          away at Hack the Box. 📦
         </p>
         <p>
-            You can find me on Twitter, scrutinise my code at Github or get in touch via LinkedIn.
+          You can find me on Twitter, scrutinise my code at Github or get in
+          touch via LinkedIn.
         </p>
       </section>
     </>
-  )
+  );
 }
