4e03266d9e2ed80695fc649e483104398de82327
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-30 19:03:05 +0000

    add links

--- components/content/Content.js
+++ components/content/Content.js
@@ -7,16 +7,16 @@ export default function Content() {
         <p>
           I'm Matthew Bidewell, I'm a Software engineer with 7 years of
           experience in creating well crafted SaaS solutions using AWS. I work
-          over at Government Digital Services where I help build Digital
-          Identification. 🧑‍💻
+          over at <a href="https://www.gov.uk/">Government Digital Services</a>y
+          where I help build Digital Identification. 🧑‍💻
         </p>
         <p>
           I'm also passionate about cyber security and you can find me hacking
           away at Hack the Box. 📦
         </p>
         <p>
-          You can find me on Twitter, scrutinise my code at Github or get in
-          touch via LinkedIn.
+          You can find me on <a href="">Twitter</a>, scrutinise my code at{" "}
+          <a href="">Github</a> or get in touch via <a href="">LinkedIn</a>.
         </p>
       </section>
     </>
