ccbd962b93426c580dbfe381609d4674b76c22ee
Matthew Bidewell <matthewbidewell@gmail.com>
2022-02-27 19:57:52 +0000

    Copy change

--- components/content/Content.js
+++ components/content/Content.js
@@ -7,12 +7,12 @@ export default function Content() {
         <p>
           I'm Matthew Bidewell, I'm a Software engineer with 7 years of
           experience in creating well crafted SaaS solutions using AWS. I work
-          over at <a href="https://www.gov.uk/">Government Digital Services</a>y
-          where I help build Digital Identification. 🧑‍💻
+          over at <a href="https://www.gov.uk/">Government Digital Services</a>{" "}
+          where I help build the future of Digital Identification. 🧑‍💻
         </p>
         <p>
           I'm also passionate about cyber security and you can find me hacking
-          away at Hack the Box. 📦
+          away at <a href="https://www.hackthebox.com">Hack the box</a> 📦
         </p>
         <p>
           You can find me on <a href="">Twitter</a>, scrutinise my code at{" "}
