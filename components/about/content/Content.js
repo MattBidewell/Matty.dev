10befefb2764fde768778175f28ae5fc6d236026
MattBidewell <matthewbidewell@gmail.com>
2022-07-06 23:14:12 +0100

    Feat: Add about

---
+++ components/about/content/Content.js
@@ -0,0 +1,13 @@
+
+export default function Content() {
+  return (
+    <>
+      <section class="content">
+        <h2>About me</h2>
+        <p>about</p>
+        <h2>Work History</h2>
+        <p>history</p>
+      </section>
+    </>
+  );
+}
