053272b50de9d791741fffa36af1bfbd0602c159
MattBidewell <matthewbidewell@gmail.com>
2022-07-06 21:46:09 +0100

    Refactor: seperate out shared components to page specific

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,7 @@ export default function Home() {
 
       </Head>
       <main className="content">
-        <p className="description">
-          <Main />
-        </p>
+        <Main />
         <Content></Content>
         <BlogLinks></BlogLinks>
       </main>
