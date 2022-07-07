991741020f5998f74c295173618c16531406fc9e
MattBidewell <matthewbidewell@gmail.com>
2022-07-07 22:29:12 +0100

    feat: added skills

--- components/about/content/Content.js
+++ components/about/content/Content.js
@@ -1,12 +1,21 @@
+import Skills from "@components/about/skills/Skills";
 
 export default function Content() {
   return (
     <>
       <section class="content">
         <h2>About me</h2>
-        <p>about</p>
-        <h2>Work History</h2>
-        <p>history</p>
+        <p>
+          I'm Matt Bidewell,  I’m a jack of all trades software engineer with {`${new Date().getFullYear() - 2015}`}+ years of
+          experience in creating well crafted SaaS solutions in the cloud ☁️ for both the public and private sectors, from UK government to Adtech. I'm currently working over at
+          at <a href="https://www.gov.uk/">Government Digital Services</a> where I'm building the future of Digtial Identification.🧑‍💻
+        </p>
+        <p>
+          You can find me on <a href="https://twitter.com/MattBidewell">Twitter</a>, scrutinise my code at{" "}
+          <a href="https://www.github.com/mattbidewell">Github</a> or get in
+          touch via <a href="https://www.linkedin.com/in/matt-bidewell-a4578790/">LinkedIn</a>.
+        </p>
+        <Skills />
       </section>
     </>
   );
--- components/home/content/Content.js
+++ components/home/content/Content.js
@@ -2,17 +2,6 @@ export default function Content() {
   return (
     <>
       <section class="content">
-        <p>
-          I'm Matt Bidewell, I'm a Software engineer with {`${new Date().getFullYear() - 2015}`}+ years of
-          experience in creating well crafted SaaS solutions in the cloud ☁️. I work over
-          at <a href="https://www.gov.uk/">Government Digital Services</a> where
-          I help build the future of Digital Identification. 🧑‍💻
-        </p>
-        <p>
-          You can find me on <a href="https://twitter.com/MattBidewell">Twitter</a>, scrutinise my code at{" "}
-          <a href="https://www.github.com/mattbidewell">Github</a> or get in
-          touch via <a href="https://www.linkedin.com/in/matt-bidewell-a4578790/">LinkedIn</a>.
-        </p>
       </section>
     </>
   );
