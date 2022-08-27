bebf729ba58f1e37eeeffebeca22d140fa0c7d5c
MattBidewell <matthewbidewell@gmail.com>
2022-08-27 18:28:04 +0100

    Correct styles for main content on home page

--- components/home/content/Content.js
+++ components/home/content/Content.js
@@ -3,12 +3,17 @@ import styles from "./Content.module.css";
 export default function Content() {
   return (
     <>
-      <section class="content" className={styles.Content}>
-        <h2>Hey!</h2>
+      <section className={styles["intro-content"]}>
+        <h2>{randomEmoji()}</h2>
         I’m a software engineer that is passionate about open source software
         and software engineering, with 7+ years of experience in creating
         well-crafted SaaS solutions in the cloud ☁️.
       </section>
     </>
   );
+}
+
+function randomEmoji() {
+  const emojis = ["💀","☠️","💩","🤡","👹","👺","👻","👽","👾","🤖","👋","🤏","🖖","🤘","🤟","🧠","👨‍🎤","🕵️","📷","🐕"];
+  return emojis[Math.floor(Math.random() * Math.floor(emojis.length))];
 }
