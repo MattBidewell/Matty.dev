d92c05a8a09010a69a846b73499778cb13801751
Matthew Bidewell <matthewbidewell@gmail.com>
2022-10-20 12:08:02 +0100

    Designfix: min-height vs max-height on safari renders image always at its largest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false }
+    config.resolve.fallback = { fs: false };
     return config;
-  }
-}
+  },
+};
