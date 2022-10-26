2fcb4dce48cd481b45df2808852316fe4518a67c
Matthew Bidewell <matthewbidewell@gmail.com>
2022-10-26 18:39:13 +0100

    Feat: Add donation link for buy me a coffee

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,15 +1,18 @@
 import { useRouter } from "next/router"
+import { Remarkable } from "remarkable";
 import Head from "next/head";
+
+// components
+import HeadBlock from "@components/shared/meta/Head";
 import Footer from "@components/shared/footer/Footer";
 import TopContent from "@components/shared/topContent/TopContent";
-import { getPostBySlug, getAllPosts } from "../../lib/api"
-import { Remarkable } from "remarkable";
-import PostBody from "../../components/blog/post-body";
-import styles from './Post.module.css'
-import hljs from 'highlight.js';
-import 'highlight.js/styles/nord.css';
-import HeadBlock from "@components/shared/meta/Head";
+import PostBody from "@components/blog/body/PostBody";
+import PostFooter from "@components/blog/footer/PostFooter";
 
+import styles from "./Post.module.css";
+import hljs from "highlight.js";
+import "highlight.js/styles/nord.css";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
 
 // remarkable and highlight js setup
 const r = new Remarkable({
@@ -17,38 +20,37 @@ const r = new Remarkable({
     if (lang && hljs.getLanguage(lang)) {
       try {
         return hljs.highlight(lang, str).value;
-      } catch (err) { }
-      console.log("error rendering code block" + err.message)
+      } catch (err) {}
+      console.log("error rendering code block" + err.message);
     }
 
     try {
       return hljs.highlightAuto(str).value;
     } catch (err) {}
 
-    return ''; // use external default escaping
-  }
+    return ""; // use external default escaping
+  },
 });
 
 export default function Post({ post }) {
-  const router = useRouter()
+  const router = useRouter();
 
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <div className="container">
-      <HeadBlock/>
+      <HeadBlock />
       <main className="page-content">
         <TopContent />
-        <h1 className={styles.title}>
-          {post.title}
-        </h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.date}>{post.date}</p>
         <PostBody content={post.content} />
+        <PostFooter />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps({ params }) {
