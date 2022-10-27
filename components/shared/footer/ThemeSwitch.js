8eef6e56994a30f2df308a62ce4adf7b13270e5f
Matthew Bidewell <matthewbidewell@gmail.com>
2022-10-27 12:59:12 +0100

    Use localstorage to store previous theme state

--- components/shared/footer/ThemeSwitch.js
+++ components/shared/footer/ThemeSwitch.js
@@ -5,24 +5,39 @@ export default function ThemeSwitch() {
     <>
       <Script strategy="afterInteractive">
         {`(() => {
-              const themeContainer = document.querySelector(".theme-container");
+          function setThemeFromStorage() {
+              if (!localStorage) return;
 
-              if (!themeContainer) return;
+              const setTheme = localStorage.getItem("theme");
 
-              const themes = document.querySelectorAll(".theme");
-
-              for (const theme of themes) {
-                theme.addEventListener("click", function (e) {
-                  if (!e || !e.target) return;
-
-                  const themeValue = e.target.getAttribute("data-theme");
-
-                  document.documentElement.setAttribute("data-theme", themeValue);
-                });
+              if(setTheme) {
+                document.documentElement.setAttribute("data-theme", setTheme);
               }
+          }
+
+          function setThemeListeners() {
+            const themes = document.querySelectorAll(".theme");
+
+            for (const theme of themes) {
+              theme.addEventListener("click", function (e) {
+                if (!e || !e.target) return;
+
+                const themeValue = e.target.getAttribute("data-theme");
+
+                document.documentElement.setAttribute("data-theme", themeValue);
+                localStorage.setItem("theme", themeValue)
+              });
+            }
+          }
+
+          const themeContainer = document.querySelector(".theme-container");
+          if (!themeContainer) return;
+          setThemeFromStorage();
+          setThemeListeners();
         })()
       `}
       </Script>
     </>
   );
 }
+
