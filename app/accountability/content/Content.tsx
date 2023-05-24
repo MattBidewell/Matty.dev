08cec755b17d09fe86ed0efcc6f7f8c5124cea6c
MattBidewell <matthewbidewell@gmail.com>
2023-05-24 22:07:00 +0100

    Add data for accountability page

---
+++ app/accountability/content/Content.tsx
@@ -0,0 +1,9 @@
+export default function Content() {
+  return (
+    <>
+      <section className="content">
+        <h2>Accountability</h2>
+      </section>
+    </>
+  );
+}
---
+++ app/accountability/page.tsx
@@ -0,0 +1,9 @@
+import Content from "./content/Content";
+
+export default function Accountability() {
+  return (
+    <div className="container">
+      <Content />
+    </div>
+  );
+}
