fc7df2763700d77a82b3ad874775bf6e58ad0e51
MattBidewell <matthewbidewell@gmail.com>
2024-01-09 21:08:23 +0000

    redesign of blog links

--- app/components/home/blogLinks/BlogLinks.tsx
+++ app/components/home/blogLinks/BlogLinks.tsx
@@ -1,6 +1,5 @@
 import { Post } from "../../../../types/post";
 import Link from "next/link";
-import Image from "next/image"
 import styles from "./BlogLinks.module.css";
 
 export default function BlogLinks({
@@ -15,7 +14,11 @@ export default function BlogLinks({
       <section className="content">
         <section className={styles.bloglinks_content}>
           <h2 className={styles.title}>Blog posts</h2>
-          {createPosts(posts)}
+          <table className={styles["table-content"]}>
+            <tbody>
+              {createPosts(posts)}
+            </tbody>
+          </table>
           {hasLimit && (
             <p className={styles["more-link"]}>
               <Link href="/blog">More posts...</Link>
@@ -30,27 +33,16 @@ export default function BlogLinks({
 function createPosts(arrayOfPosts: Post[]) {
   return Array.from(arrayOfPosts).map((post) => {
     return (
-      <div className={styles["post-container"]} key={post.slug}>
-        <div className={styles["post-thumbnail"]}>
-          <picture>
-            <Image
-              src={`/assets/images/${post.image}`}
-              alt={post.alt}
-              fill
-              placeholder="blur"
-              blurDataURL={`/assets/images/${post.image}`}
-            />
-          </picture>
-        </div>
-        <div className={styles["post-container-content"]}>
-          <h3 className="post-link-title">{post.title}</h3>
-          <p className="date">{post.date}</p>
-          <p>{post.excerpt}</p>
+      <tr key={post.linkSlug}>
+        <td className={styles["date-cell"]}>
+          <p>{post.date}</p>
+        </td>
+        <td>
           <p>
-            <Link href={post.linkSlug}>Read more...</Link>
+            <Link href={post.linkSlug}>{post.title}</Link>
           </p>
-        </div>
-      </div>
+        </td>
+      </tr>
     );
   });
 }
